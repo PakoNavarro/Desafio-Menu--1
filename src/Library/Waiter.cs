9fdf6d1a12de20c0edf08dbc1ecf0087a24a049a
PakoNavarro <pakonavarropako@gmail.com>
2026-08-20 18:51:46 -0300

    Implementacion Clase Waiter

--- src/Library/Waiter.cs
+++ src/Library/Waiter.cs
@@ -14,5 +14,39 @@ namespace Ucu.Poo.Restaurant
     public class Waiter
     {
         private ArrayList assignedTables = new ArrayList();
+
+
+        /// <summary>
+        /// Obtiene el nombre del mozo.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Crea un mozo con el nombre indicado.
+        /// </summary>
+        /// <param name="name">El nombre del mozo.</param>
+        public Waiter(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// Asigna una mesa al mozo.
+        /// </summary>
+        /// <param name="table">La mesa a asignar.</param>
+        public void AssignTable(Table table)
+        {
+            assignedTables.Add(table);
+        }
+
+        /// <summary>
+        /// Toma un pedido para una mesa específica y agrega el platillo a la orden de esa mesa.
+        /// </summary>
+        /// <param name="table">La mesa para la que se toma el pedido.</param>
+        /// <param name="dish">El platillo pedido.</param>
+        public void TakeOrder(Table table, Dish dish)
+        {
+            table.AddToOrder(dish);
+        }
     }
 }
