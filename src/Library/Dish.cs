adfa37800ed557b2cec67d9a5d739a613edc9f10
PakoNavarro <pakonavarropako@gmail.com>
2026-08-20 12:21:04 -0300

    Implementacion Clase Dish

--- src/Library/Dish.cs
+++ src/Library/Dish.cs
@@ -11,5 +11,17 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Dish
     {
+        public string Name { get; set;}
+
+        public double Price { get; set;}
+
+        public bool IsVegetarian { get; set;}
+
+        public Dish(string name, double price, bool isVegetarian)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.IsVegetarian = isVegetarian;
+        }
     }
 }
