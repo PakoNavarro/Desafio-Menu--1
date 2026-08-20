c6734c3daada89150a47cd0b2b3a60d19ef31b8c
PakoNavarro <pakonavarropako@gmail.com>
2026-08-20 17:46:31 -0300

    Implementacion Clase Menu

--- src/Library/Menu.cs
+++ src/Library/Menu.cs
@@ -15,5 +15,33 @@ namespace Ucu.Poo.Restaurant
     public class Menu
     {
         private ArrayList dishes = new ArrayList();
+        
+        /// <summary>
+        /// Agrega un platillo a la lista de platillos del menú.
+        /// </summary>
+        /// <param name="dish">El platillo a agregar.</param>
+        public void AddDish(Dish dish)
+        {
+            dishes.Add(dish);
+        }
+        /// <summary>
+        /// Elimina un platillo de la lista del menú.
+        /// </summary>
+        /// <param name="dish">El platillo a eliminar.</param>
+        public void RemoveDish(Dish dish)
+        {
+            dishes.Remove(dish);
+        }
+        public Dish GetDishByName(string name)
+        {
+            foreach (Dish dish in dishes)
+            {
+                if (dish.Name == name)
+                {
+                    return dish;
+                }
+            }
+            return null;
+        }
     }
 }
